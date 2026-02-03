ec02326a178db63dfcbc1480db369f7c012a7f24
Pownek-code <lizzotuco0@gmail.com>
2026-02-03 13:59:10 -0800

    flake8 py01/ex01

--- python01/ex01/ft_garden_data.py
+++ python01/ex01/ft_garden_data.py
@@ -1,19 +1,18 @@
 class Plant:
-	def __init__(self ,name: str, height: int, age: int):
-		self.name = name
-		self.height = height
-		self.age = age
-	def __str__(self):
-		return f"{self.name}: {self.height}cm, {self.age} days old"
+    def __init__(self, name: str, height: int, age: int):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}: {self.height}cm, {self.age} days old"
+
 
 def main():
-	print(Plant("Rose", 5, 3))
-	print(Plant("ophelia", 2, 5))
-	print(Plant("darwin", 7, 2))
+    print(Plant("Rose", 5, 3))
+    print(Plant("ophelia", 2, 5))
+    print(Plant("darwin", 7, 2))
+
 
 if __name__ == "__main__":
-	main()
-# 	print(Plant("Rose", 5, 3))
-# 	print(Plant("ophelia", 2, 5))
-# 	print(Plant("darwin", 7, 2))
-
+    main()
