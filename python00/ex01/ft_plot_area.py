616fc59ba71847ec6e4a73f4d1da1caf5681db9c
Pownek-code <lizzotuco0@gmail.com>
2026-02-03 16:06:26 +0100

    ex01 fixed flake

--- python00/ex01/ft_plot_area.py
+++ python00/ex01/ft_plot_area.py
@@ -1,2 +1,3 @@
 def ft_plot_area():
-	print(f"Plot area: {int(input("Enter length: ")) * int(input("Enter width: "))}")
+    plot_area = int(input("Enter length: "))
+    print(f"Plot area: {plot_area * int(input("Enter width: "))}")
