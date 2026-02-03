72f7d8200cfec1759e8191bc014410b4848ffbdb
Pownek-code <lizzotuco0@gmail.com>
2026-02-03 07:27:58 -0800

    ex03 v2

--- python00/ex03/ft_plant_age.py
+++ python00/ex03/ft_plant_age.py
@@ -1,3 +1,6 @@
 def ft_plant_age():
 	age = int(input("Enter plant age in days: "))
-	print(f"{"Plant is ready to harvest!" if age > 60 else "Plant needs more time to grow."}")
+	if age > 60:
+		print("Plant is ready to harvest!")
+	else:
+		print("Plant needs more time to grow.")
