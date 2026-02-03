1899250693ee54b377f9d18e37e2cc9ee1dacd51
Pownek-code <lizzotuco0@gmail.com>
2026-02-03 14:00:34 -0800

    flake8 py1/ex02

--- python01/ex02/ft_plant_growth.py
+++ python01/ex02/ft_plant_growth.py
@@ -1,21 +1,19 @@
 class Plant:
-	def __init__(self ,name: str, init_height: int, init_age: int):
-		self.name = name
-		self.height = init_height
-		self.days_old = init_age
-	def grow(self, cm = 1):
-		self.height += cm
-	def age(self):
-		self.days_old += 1
-	def get_info(self) -> str:
-		return f"{self.name}, {self.height}, {self.days_old}"
-		
+    def __init__(self, name: str, init_height: int, init_age: int):
+        self.name = name
+        self.height = init_height
+        self.days_old = init_age
 
+    def grow(self, cm = 1):
+        self.height += cm
+    def age(self):
+        self.days_old += 1
+    def get_info(self) -> str:
+        return f"{self.name}, {self.height}, {self.days_old}"
 
 
 if __name__ == "__main__":
-	p = Plant("Rose", 5, 3)
-		
-	p.grow()
-	p.age()
-	print(p.get_info())
+    p = Plant("Rose", 5, 3)
+    p.grow()
+    p.age()
+    print(p.get_info())
