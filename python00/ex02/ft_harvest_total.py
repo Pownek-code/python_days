224523dce31b58884d52860892bb7d7b1ea76858
Pownek-code <lizzotuco0@gmail.com>
2026-02-03 07:15:56 -0800

    ex02 fixed

--- python00/ex02/ft_harvest_total.py
+++ python00/ex02/ft_harvest_total.py
@@ -1,2 +1,5 @@
 def ft_harvest_total():
-	print(f"Total harvest: {int(input("Day 1 harvest: ")) + int(input("Day 2 Harvest: ")) + int(input("Day 3 Harvest: "))}")
+	day1 = int(input("Day 1 harvest: "))
+	day2 = int(input("Day 2 Harvest: "))
+	day3 = int(input("Day 3 Harvest: "))
+	print(f"Total Harvest: {day1 + day2 + day3}")
